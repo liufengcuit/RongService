7f7278cfc5397494e1ed347dda50408d7d71bb4f
liufeng <liufeng1@xksquare.com>
2019-05-29 00:20:53 +0800

    token验证

--- token/index.js
+++ token/index.js
@@ -3,7 +3,7 @@ const secret  = 'liufeng';
 
 const token = {
 	createToken: function(payload) {
-		const _token = jwt.sign(payload, secret, { expiresIn: '3000' })
+		const _token = jwt.sign(payload, secret, { expiresIn: '3000' })       //可设置1h 1days 1d  默认为ms
 		return _token;
 	}
 }
